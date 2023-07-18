d81ee927c7b4ceaf41952b14b69eea27084ba538
Marta Dziegielewska <marta.dziegielwska@lppsa.com>
2023-07-18 21:37:51 +0200

    add visualization and add to utils

--- utils.py
+++ utils.py
@@ -7,6 +7,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
+import math
+from albumentations import CenterCrop, RandomRotate90, GridDistortion, HorizontalFlip, VerticalFlip
 
 
 def stabilize_video(input_path, output_path):
@@ -24,11 +26,10 @@ def stabilize_video(input_path, output_path):
 
 
 def convert_video_to_frames(video_path, output_path):
-    base_dir = os.path.realpath(video_path)
-    print("converting video from ", base_dir)
+    print("converting video from ", video_path)
 
     # read video
-    vidcap = cv2.VideoCapture(base_dir)
+    vidcap = cv2.VideoCapture(video_path)
     ret,image = vidcap.read()
     fps = vidcap.get(cv2.CAP_PROP_FPS)
 
@@ -62,10 +63,9 @@ def convert_timestamp_to_frame(video_path, timestamp):
 
 
 def convert_frames_to_video(frames_path, video_name, fps):
-    base_dir = os.path.realpath(frames_path)
-    print("converting frames from ", base_dir)
+    print("converting frames from ", frames_path)
 
-    file_list = glob.glob(f'{base_dir}/*.png')  # get all the pngs in the current directory
+    file_list = glob.glob(f'{frames_path}/*.png')  # get all the pngs in the current directory
     file_list_sorted = natsorted(file_list,reverse=False)
 
     clips = [ImageClip(m).set_duration(0.03)
@@ -76,12 +76,9 @@ def convert_frames_to_video(frames_path, video_name, fps):
 
 
 def read_files(path):
-    base_dir = os.path.realpath(path)
-    print(base_dir)
-
     files = []
 
-    file_list = glob.glob(f"{base_dir}/*.*")  # get all the filesin the current directory
+    file_list = glob.glob(f"{path}/*.*")  # get all the filesin the current directory
     file_list_sorted = natsorted(file_list,reverse=False)
 
     for file in file_list_sorted:
@@ -93,30 +90,113 @@ def read_files(path):
     return files
 
 
-def read_image_files(path, H, W):
-    base_dir = os.path.realpath(path)
-    print(base_dir)
-
+def read_image_files(path, H, W, interpolation=False):
     files = []
 
-    file_list = glob.glob(f"{base_dir}/*.png")  # get all the pngs in the current directory
+    file_list = glob.glob(f"{path}/*.*")  # get all the pngs in the current directory
     file_list_sorted = natsorted(file_list,reverse=False)
 
-    for file in file_list_sorted:
-        img = cv2.imread(file, 1)
-        img = cv2.resize(img, (H, W))
-        files.append(img)
+    if interpolation == False:
+        for file in file_list_sorted:
+            img = cv2.imread(file, 1)
+            img = cv2.resize(img, (H, W))
+            files.append(img)
+    else:
+        for file in file_list_sorted:
+            img = cv2.imread(file, 0)
+            img = cv2.resize(img, (H, W), interpolation = cv2.INTER_NEAREST)  # otherwise ground truth changes 
+            files.append(img)
 
-    files = np.array(files)
+    files_arr = np.array(files)
     len(f"read {len(files)} images")
 
-    return files
+    return files_arr
 
 
-if __name__ == "__main__":
-   # function to run
-   directory = "/media/madziegielewska/Seagate Expansion Drive/MAGISTERKA/diploma-project"
-   #convert_video_to_frames(f"{directory}/videos/21_Trim2.mp4", "frames")
+def get_average_pixels(mask_list, return_weights=False):
+    background = 0
+    needle = 0
+    oocyte = 0
+    spermatozoid = 0
 
-   frame = convert_timestamp_to_frame(f"{directory}/videos/100_Trim1.mp4", "00:00:16.704")
-   print(frame)
+    for y in mask_list:
+        background += y.size - np.count_nonzero(y)
+        needle += np.count_nonzero(y == 1)
+        oocyte += np.count_nonzero(y == 2)
+        spermatozoid += np.count_nonzero(y == 3)
+
+    # get average pixels on mask
+    print("Needle: ", round(needle/len(mask_list)))
+    print("Ooctye: ", round(oocyte/len(mask_list)))
+    print("Spermatozoid: ", round(spermatozoid/len(mask_list)))
+    print("Background: ", round(background/len(mask_list)))
+    print()
+
+    if return_weights==True:
+        return get_weights(background, needle, oocyte, spermatozoid)
+
+
+def get_weights(b, n, o, s):
+    classes_count_mean = [0,0,0,0]
+
+    classes_count_mean[0] = b
+    classes_count_mean[1] = n
+    classes_count_mean[2] = o
+    classes_count_mean[3] = s
+
+    classes_count_mean = [1/x for x in classes_count_mean]
+    classess_weights = classes_count_mean / np.linalg.norm(classes_count_mean)
+
+    return classess_weights
+
+
+def augment_data(images, masks, size_x, size_y):
+    augmented_images = []
+    augmented_masks = []
+
+    # iterate over list of images and masks
+    for x, y in zip(images, masks):
+        augmented_images.append(x)
+        augmented_masks.append(y)
+
+        aug = CenterCrop(size_x, size_y, p=1.0)
+        augmented = aug(image=x, mask=y)
+        x1 = augmented['image']
+        y1 = augmented['mask']
+
+        augmented_images.append(x1)
+        augmented_masks.append(y1)
+
+        aug = RandomRotate90(p=1.0)
+        augmented = aug(image=x, mask=y)
+        x2 = augmented['image']
+        y2 = augmented['mask']
+
+        augmented_images.append(x2)
+        augmented_masks.append(y2)
+
+        aug =GridDistortion(p=1.0)
+        augmented = aug(image=x, mask=y)
+        x3 = augmented['image']
+        y3 = augmented['mask']
+
+        augmented_images.append(x3)
+        augmented_masks.append(y3)
+
+        aug = HorizontalFlip(p=1.0)
+        augmented = aug(image=x, mask=y)
+        x4 = augmented['image']
+        y4 = augmented['mask']
+
+        augmented_images.append(x4)
+        augmented_masks.append(y4)
+
+        aug = VerticalFlip(p=1.0)
+        augmented = aug(image=x, mask=y)
+        x5 = augmented['image']
+        y5 = augmented['mask']
+
+        augmented_images.append(x5)
+        augmented_masks.append(y5)
+
+    return augmented_images, augmented_masks
