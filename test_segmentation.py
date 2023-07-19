c3ac61e3d34434ed4ef573a9e1a48934e5d5fe22
Marta Dziegielewska <marta.dziegielwska@lppsa.com>
2023-07-19 15:39:34 +0200

    add py file to test segmentation

---
+++ test_segmentation.py
@@ -0,0 +1,78 @@
+# imports
+
+import os
+import cv2
+import numpy as np
+import glob
+import utils
+import tensorflow as tf
+from keras.models import load_model
+from moviepy.editor import *
+from natsort import natsorted
+os.environ["SM_FRAMEWORK"] = "tf.keras"
+import segmentation_models as sm
+import matplotlib.pyplot as plt
+
+print(tf.config.list_physical_devices('GPU'))
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+
+
+def test_segmentation(input_path, output_path, model, backbone, statistics=True):
+    frames = utils.read_image_files(input_path, 384, 384)
+
+    model = load_model(model, compile=False)
+    preprocess_input = sm.get_preprocessing(backbone)
+
+    results = []
+
+    for frame in frames:
+        frame_input = np.expand_dims(frame, 0)
+        test_frame = preprocess_input(frame_input)
+        test = model.predict(test_frame)
+        test_pred = np.argmax(test, axis=3)[0,:,:]
+
+        results.append(test_pred)
+
+    preds = np.array(results)
+
+    if statistics == True:
+        n = np.random.randint(len(preds)-1)
+
+        stats = utils.get_average_pixels(preds)
+
+        plt.figure(figsize=(12, 8))
+        plt.subplot(231)
+        plt.title('Original frame')
+        plt.imshow(frames[n], vmin=0, vmax=3, cmap='gray')
+        plt.subplot(232)
+        plt.title('Prediction mask')
+        plt.imshow(preds[n], vmin=0, vmax=3, cmap='jet')
+        plt.subplot(233)
+        plt.title('Prediction mask on image')
+        plt.imshow(frames[n], vmin=0, vmax=3, cmap='gray')
+        plt.imshow(preds[n], vmin=0, vmax=3, cmap="jet", alpha=0.3)
+        plt.show()
+
+
+    count = 0
+
+    for i,m in zip(frames, preds):
+        spermatozoid_mask = m.astype(np.uint8)
+        spermatozoid_mask[spermatozoid_mask != 3] = 0
+        dst = cv2.bitwise_not(i,i, spermatozoid_mask)
+
+        cv2.imwrite(f"{output_path}/frame_{count}.png", dst)
+        count += 1
+
+    utils.convert_frames_to_video(output_path, "test_unet.mp4", 20)
+
+
+if __name__ == "__main__":
+    directory = "/media/madziegielewska/Seagate Expansion Drive/MAGISTERKA/diploma-project/Semantic-Segmentation"
+    
+    input_path = f"{directory}/frames_to_test_segmentation"
+    output_path = f"{directory}/segmentation_test_results"
+
+    model = f"{directory}/models/psp_softmax_1500_resnet50.hdf5"
+
+    test_segmentation(input_path, output_path, model, "resnet50")
