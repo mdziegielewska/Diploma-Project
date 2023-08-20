c5a367a9761b513c407f6ad77fbef122b99f7d45
Marta Dziegielewska <marta.dziegielwska@lppsa.com>
2023-08-20 11:09:56 +0200

    finished app

--- Demo-App/main.py
+++ Demo-App/main.py
@@ -5,25 +5,40 @@ from app import app
 import urllib.request
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
-	
+
 @app.route('/')
 def upload_form():
 	return render_template('upload.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/results', methods=['GET','POST'])
 def upload_video():
 	if 'file' not in request.files:
-		flash('No file part')
+		flash('Brak pliku bądź rozwiązania')
 		return redirect(request.url)
 	file = request.files['file']
+	selected_option = request.form.get('dropdown')
+
 	if file.filename == '':
 		flash('No image selected for uploading')
 		return redirect(request.url)
 	else:
+		if selected_option == 'move':
+			current_res = 1
+			result_num = 2
+		elif selected_option == 'event':
+			current_res = 2
+			result_num = 1
+
 		filename = secure_filename(file.filename)
 		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		# print('upload_video filename: ' + filename)
-		return render_template('results.html', filename=filename)
+		return render_template('results.html', filename=filename, curr=current_res, result=result_num)
+
+@app.route('/results/filename=<filename>/current_res=<current_res>')
+def get_new(filename, current_res):
+	if current_res == '1':
+		return render_template('results.html', filename=filename, curr=2, result=current_res)
+	elif current_res == '2':
+		return render_template('results.html', filename=filename, curr=1, result=current_res)
 
 @app.route('/display/<filename>')
 def display_video(filename):
