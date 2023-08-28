7b6c75c8e519f288afe99d2161c7308bede84ce4
Marta Dziegielewska <marta.dziegielwska@lppsa.com>
2023-08-28 06:56:42 +0200

    add transnetv1 tests

--- Demo-App/app.py
+++ Demo-App/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 UPLOAD_FOLDER = '/media/madziegielewska/Seagate Expansion Drive/MAGISTERKA/diploma-project/Demo-App/static/uploads'
 
 app = Flask(__name__)
+
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
